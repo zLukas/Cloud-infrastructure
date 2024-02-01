ea5840a959cdddc31a05628a9df85ff6e57491bd
zLukas <xlukas9@gmail.com>
2024-02-01 14:49:04 +0100

    invoke URL added to lambda module

--- aws/lambda/main.tf
+++ aws/lambda/main.tf
@@ -15,3 +15,12 @@ resource "aws_lambda_function" "lambda" {
     variables = var.env_vars
       }
 }
+
+
+resource "aws_lambda_function_url" "invoke_url" {
+  function_name      = var.lambda_name
+  authorization_type = "AWS_IAM"
+  depends_on = [
+    aws_lambda_function.lambda
+  ]
+}
--- aws/lambda/outputs.tf
+++ aws/lambda/outputs.tf
@@ -5,3 +5,8 @@ output lambda_invoke_arn {
 output arn {
     value = aws_lambda_function.lambda.arn
 }
+
+output lambda_invoke_url {
+    value = aws_lambda_function_url.clientLambda.function_url
+}
+}
